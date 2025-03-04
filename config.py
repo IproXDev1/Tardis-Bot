1689e0b3f0fc928d64823be3433a4b8890f20d79
IproXDev <110981752+IproXDev1@users.noreply.github.com>
2025-03-04 10:35:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    "TOKEN": "MTEyMDgyMzY4MzY4MjkzNDg1NA.GKIp25.Pokyy_LAMKoLhcc46GnDAWo5YcsqGDNiImQj9c", #change par ton token
+    "TOKEN": "ton_token", #change par ton token
     "PREFIX": "!",
     "SERVER_IP": "node02.fardihosting.fr",  # Adresse du serveur Minecraft
     "SERVER_PORT": 25312,  # Port Minecraft
